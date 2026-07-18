c66a3421f59770e5295fa8fe15042b72307e2972
Abinav <abinav0412@gmail.com>
2026-07-18 11:42:03 +0530

    Use Firebase Admin modular API

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
 
-const admin = require("firebase-admin");
+const { initializeApp, cert } = require("firebase-admin/app");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
+initializeApp({
+  credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
   })
 });
-
 const express = require("express");
 const cors = require("cors");
 const SibApiV3Sdk = require("sib-api-v3-sdk");
