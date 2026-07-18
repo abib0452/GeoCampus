77c4947677b7c245a9236447da271637a2ed74cb
Abinav <abinav0412@gmail.com>
2026-07-18 11:02:58 +0530

    Fix Firebase Admin initialization

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
-const { initializeApp, cert } = require("firebase-admin/app");
-
-const serviceAccount = require("./admin.json");
-
-initializeApp({
-    credential: cert(serviceAccount)
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  })
 });
 
 const express = require("express");
